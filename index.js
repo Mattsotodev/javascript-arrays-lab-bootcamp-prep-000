d6056b4ec5a28ab7105488809c80c8cd2a3770f1
Matthew Soto <matthewsoto95@gmail.com>
2019-04-17 00:21:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ function appendKitten(name){
 function prependKitten(name){
   return ['Arnold', ...kittens];
 }
-function removeLastKitten(){ 
-  return
+
+ function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+ function removeFirstKitten() {
+  return kittens.slice(1);
 }
